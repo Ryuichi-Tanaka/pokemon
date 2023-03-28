0c3cede04c6560a8854491b223db9ea6846bc0fb
ryuichitanaka <tuutiyou.otameshi@gmail.com>
2023-03-28 21:07:58 +0900

    second

---
+++ pages/バイオレット.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import streamlit as st
+
+
+st.write("バイオレットex買取")
+
+if st.checkbox("相場の確認"):
+    # スクレイピング対象のURLを指定
+    url = "https://pokemon-infomation.com/market-price-violetex/"
+
+    # GETリクエストを送信してHTMLを取得
+    response = requests.get(url)
+
+    # HTMLをパースしてBeautifulSoupオブジェクトを作成
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # スクレイピング対象の要素を取得
+    target_element = soup.find("tr")
+
+    for a in soup.find_all("tr"):
+        st.write(a.text)

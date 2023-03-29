3b4d64076fac9ccee426b26fb659a3f3c2c756b5
ryuichitanaka <tuutiyou.otameshi@gmail.com>
2023-03-30 00:09:33 +0900

    ten

--- pages/07_ロストアビス.py
+++ pages/07_ロストアビス.py
@@ -6,21 +6,21 @@ import streamlit as st
 
 st.write("ロストアビス買取")
 
-if st.checkbox("相場の確認"):
-    # スクレイピング対象のURLを指定
-    url = "https://pokemon-infomation.com/market-price-lostabys/"
 
-    # GETリクエストを送信してHTMLを取得
-    response = requests.get(url)
+# スクレイピング対象のURLを指定
+url = "https://pokemon-infomation.com/market-price-lostabys/"
 
-    # HTMLをパースしてBeautifulSoupオブジェクトを作成
-    soup = BeautifulSoup(response.text, "html.parser")
+# GETリクエストを送信してHTMLを取得
+response = requests.get(url)
 
-    # スクレイピング対象の要素を取得
-    target_element = soup.find("tr")
+# HTMLをパースしてBeautifulSoupオブジェクトを作成
+soup = BeautifulSoup(response.text, "html.parser")
 
-    for a in soup.find_all("tr"):
-        if "販売価格" in a.text:
-            break
-        else:
-            st.write(a.text)
+# スクレイピング対象の要素を取得
+target_element = soup.find("tr")
+
+for a in soup.find_all("tr"):
+    if "販売価格" in a.text:
+        break
+    else:
+        st.write(a.text)
--- pages/13_イーブイヒーローズ.py
+++ pages/13_イーブイヒーローズ.py
@@ -4,11 +4,11 @@ import pandas as pd
 import streamlit as st
 
 
-st.write("VSTERユニバース買取")
+st.write("イーブイヒーローズ買取")
 
 
 # スクレイピング対象のURLを指定
-url = "https://pokemon-infomation.com/market-price-vstaruniverse/"
+url = "https://osomatsusan.hatenablog.com/entry/pokekaibui"
 
 # GETリクエストを送信してHTMLを取得
 response = requests.get(url)
@@ -16,11 +16,7 @@ response = requests.get(url)
 # HTMLをパースしてBeautifulSoupオブジェクトを作成
 soup = BeautifulSoup(response.text, "html.parser")
 
-# スクレイピング対象の要素を取得
-target_element = soup.find("tr")
-
-for a in soup.find_all("tr"):
-    if "販売価格" in a.text:
-        break
-    else:
-        st.write(a.text)
+# スクin targett:レイピング対象の要素を取得
+target = soup.find("ol")
+for i in soup.find("ol"):
+    st.write(i.text)
